d123b379639b7984a8949edf08e3ab329a843666
westcripp <korukaltan@gmail.com>
2014-05-22 19:34:15 +0300

    Base: App Circle sidebar [1/2]

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1843,6 +1843,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             }
         } else if (!mRecreating) {
             // usual case: status bar visible & not immersive
+            addAppCircleSidebar();
 
             // show the ticker if there isn't already a heads up
             if (mInterruptingNotificationEntry == null) {
