ee66fec7648a18674ce2fb97f3b753c97d6b15aa
dankoman <dankoman30@gmail.com>
2014-06-26 02:53:30 +0200

    FB: Slimrecents background color && transparency [2/2]
    Change-Id: Icf0259ce45d416364d6e7a7a8c2038f6c4c0cbde

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4760,6 +4760,13 @@ public final class Settings {
         public static final String WHITELIST_APP_CIRCLE_BAR = "whitelist_app_circle_bar";
 
         /**
+         * Recent panel background color
+         *
+         * @hide
+         */
+        public static final String RECENT_PANEL_BG_COLOR = "recent_panel_bg_color";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
