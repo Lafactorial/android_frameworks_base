c88ef62e869adefe1750ac0ab8b8d06c6aabaf7b
david <davidteri91@gmail.com>
2014-08-16 07:18:27 +0200

    Revert "FB: Slimrecents background color && transparency [2/2]"
    This reverts commit ee66fec7648a18674ce2fb97f3b753c97d6b15aa.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4789,13 +4789,6 @@ public final class Settings {
         public static final String WHITELIST_APP_CIRCLE_BAR = "whitelist_app_circle_bar";
 
         /**
-         * Recent panel background color
-         *
-         * @hide
-         */
-        public static final String RECENT_PANEL_BG_COLOR = "recent_panel_bg_color";
-
-        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
